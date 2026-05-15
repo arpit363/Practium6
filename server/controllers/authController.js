8c9e7b0900abdfcee3a848fcf9361bb214ab9a2b
Arpit Chhabra <arpitchhabra9692@gmail.com>
2026-05-15 10:01:08 +0530

    fix: force IPv4 for nodemailer to fix ENETUNREACH on Render

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false, // true for 465, false for other ports
+  family: 4, // Force IPv4 routing (Render free tier does not support IPv6)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
