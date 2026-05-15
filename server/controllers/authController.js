0f8d8db8a372cae85d086f724cec85877c25ba90
Arpit Chhabra <arpitchhabra9692@gmail.com>
2026-05-15 09:49:30 +0530

    fix: change nodemailer port to 587 to fix render timeout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,7 +6,9 @@ import crypto from 'crypto';
 
 // Setup nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
